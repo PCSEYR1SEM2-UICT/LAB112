a8ee27c39863555cc23d3fefa2927dbab5ff38fb
LAB112 <whycliffbarclaynes@gmail.com>
2026-05-03 17:13:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //     e.g. lab112-v3 → lab112-v4
 //     This forces all installed apps to download the latest version.
 
-const CACHE_NAME = 'lab112-v29';
+const CACHE_NAME = 'lab112-v30';
 
 const CORE_ASSETS = [
   '/HomePage.html',
